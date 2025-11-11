b0c2b52cef7e8768e288a597f6fa9028d546a136
7venCuts <mhd866811@gmail.com>
2025-11-11 19:40:00 +0300

    Generate instruction builders for NCash operations

---
+++ sdk/src/generated/instructions/index.ts
@@ -0,0 +1,6 @@
+export * from './addEpoch'
+export * from './changeEpochIndexEpochConfig'
+export * from './createDapp'
+export * from './createGroup'
+export * from './initializeEpochConfig'
+export * from './verifyProof'
