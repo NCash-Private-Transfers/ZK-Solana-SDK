d088cc5cce248d4df311f74ed363dc14b0919279
7venCuts <mhd866811@gmail.com>
2025-11-17 13:10:00 +0300

    Add script utilities, SDK solita config, and test entrypoint

---
+++ sdk/.solitarc.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const programDir = path.join(__dirname, "..", "programs/ncash");
+const idlDir = path.join(__dirname, "idl");
+const sdkDir = path.join(__dirname, "src", "generated");
+const binaryInstallDir = path.join(__dirname, ".crates");
+
+module.exports = {
+  idlGenerator: "anchor",
+  programName: "ncash",
+  programId: "2NRByeqyVqXf4LByQP8aTAnWToK9zCwV8JSBZTW2gQAq",
+  idlDir,
+  sdkDir,
+  binaryInstallDir,
+  programDir,
+};
